af7746052c48a233e24fd97769ddec9253a62aa3
Ardit Krasniqi <krasniqiardit01@gmail.com>
2018-09-20 10:12:35 +0200

    added readme.md

---
+++ README.md
@@ -0,0 +1,8 @@
+# Chat-Me
+
+#### Simple Web App Chat using web sockets
+
+#### Technologies used
+  - Expressjs
+  - Angular 6
+  - socket.io
--- server/index.js
+++ server/index.js
@@ -1,24 +1,17 @@
-var express = require('express'); // look for express in file
+var express = require('express');
 var socket = require('socket.io');
 
-// App setup
 var app = express();
 var port = process.env.PORT || 9000
 var server = app.listen(port, function () {
     console.log('server running at port: ' + port);
 });
 
-// Static files
 app.use(express.static('public'));
 
-// Socket setup
 var io = socket(server);
 io.on('connection', function (socket) {
     console.log('Socket connection:', socket.id);
-
-    // Handle chat event
-    // When I hear chat message sent to me, fire callback 
-    // function to receive data and pass function
     socket.on('chat', function (data) {
         io.sockets.emit('chat', data);
     });
